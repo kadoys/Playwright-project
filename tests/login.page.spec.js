645749e1773e0e5bc2668d9fd42445ebf01b682e
Valeriia Horiuchkina <80711623+kadoys@users.noreply.github.com>
2025-02-25 16:08:54 +0100

    new commit

--- tests/login.page.spec.js
+++ tests/login.page.spec.js
@@ -1,70 +1,67 @@
-const { test, expect } = require('@playwright/test');
 
-const BASE_URL = 'https://accounts.kw.com';
-const VALID_EMAIL = 'kadoys@gmail.com';
-const VALID_PASSWORD = 'Kriscolfer1!';
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pages/LoginPage'); 
+
+const BASE_URL = 'https://kw.com/';
+const VALID_EMAIL = 'Valeriia.Horiuchkina@ventionteams.com';
+const VALID_PASSWORD = 'Kriscolfer654321!';
 const INVALID_EMAIL = 'invalid@example';
 const INVALID_PASSWORD = 'wrongpass';
 
 //Successful login
 
 test('Successful login with valid credentials', async ({ page }) => {
-    await page.goto(BASE_URL);
-    await page.fill('input[name="email"]', VALID_EMAIL);
-    await page.fill('input[name="password"]', VALID_PASSWORD);
-    await page.click('button:has-text("Log In")');
-    await expect(page).toHaveURL(/dashboard/); // Проверяем, что редирект на dashboard
+ let loginPage = new LoginPage(page);
+ await loginPage.navigate();
+ await loginPage.login(VALID_EMAIL, VALID_PASSWORD);
+ await expect(page).toHaveURL(BASE_URL);
+
 });
+ 
 
 //Login fails
 
 test('Login fails with incorrect password', async ({ page }) => {
-    await page.goto(BASE_URL);
-    await page.fill('input[name="email"]', VALID_EMAIL);
-    await page.fill('input[name="password"]', INVALID_PASSWORD);
-    await page.click('button:has-text("Log In")');
-    await expect(page.locator('.error-message')).toHaveText('Incorrect email or password');
+    let loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login(VALID_EMAIL, INVALID_PASSWORD);
+    await expect(await loginPage.getErrorMessage()).toContain('Invalid log in credentials. Please verify the email and password'); ;
 });
 
-//Login fails with invalid email
-
-test('Login fails with invalid email format', async ({ page }) => {
-    await page.goto(BASE_URL);
-    await page.fill('input[name="email"]', INVALID_EMAIL);
-    await page.fill('input[name="password"]', VALID_PASSWORD);
-    await page.click('button:has-text("Log In")');
-    await expect(page.locator('.error-message')).toHaveText('Invalid email format');
+test('Login fails with incorrect email', async ({ page }) => {
+    let loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login(INVALID_EMAIL, VALID_PASSWORD);
+    await expect(await loginPage.getErrorMessage()).toContain('Invalid log in credentials. Please verify the email and password'); ;
 });
+
 
 // When fields are empty
 
 test('Login button is disabled when fields are empty', async ({ page }) => {
-    await page.goto(BASE_URL);
-    const loginButton = page.locator('button:has-text("Log In")');
-    await expect(loginButton).toBeDisabled();
+    let loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login('','');
+    await expect(loginPage.logInDiv).toBeDisabled();
 });
 
 // Forget password page
 
 test('Forgot password link redirects to reset page', async ({ page }) => {
-    await page.goto(BASE_URL);
-    await page.click('text=Forgot Password?');
-    await expect(page).toHaveURL(/forgot-password/);
+    let loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.clickForgotPassword();
+    await expect.toHaveText("Forgot Password?");
 });
 
 // Google authorization
-
+/*
 test('Login with Google button works', async ({ page }) => {
     await page.goto(BASE_URL);
     await page.click('button:has-text("Google")');
     await expect(page).toHaveURL(/accounts.google.com/);
 });
+*/
 
-// 'Password input field hides text
+//'Password input field hides text
 
-test('Password input field hides text', async ({ page }) => {
-    await page.goto(BASE_URL);
-    const passwordInput = page.locator('input[name="password"]');
-    await passwordInput.fill(VALID_PASSWORD);
-    await expect(passwordInput).toHaveAttribute('type', 'password');
-});
